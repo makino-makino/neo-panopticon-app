c99242b2140e0ab9fa11cd14455f70606ea1b40a
Ami Numazaki <>
2019-11-08 03:54:00 +0900

    tabbarつくった

---
+++ components/TabBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div id="tabbar">
+            <p id="tabbar__title">{{title}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['title']
+}
+</script>
+
+<style>
+    #tabbar{
+        background-color: yellow;
+        height: 80px;
+        width: 100%;
+    }
+    #tabbar__title{
+        text-align: center;
+        line-height: 80px;
+    }
+</style>
