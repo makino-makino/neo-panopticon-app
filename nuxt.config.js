7b347810134ccd4fb988158164a332adb42cedc9
akakou <akama@keio.jp>
2019-11-13 00:13:20 +0900

    nuxtのproxyをインストール

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,13 +40,23 @@ export default {
     'bootstrap-vue/nuxt',
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
-    '@nuxtjs/pwa'
+    '@nuxtjs/pwa',
+    '@nuxtjs/proxy'
   ],
   /*
   ** Axios module configuration
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
+  },
+
+  proxy: {
+    '/api': {
+      target: 'http://api:3000/',
+      pathRewrite: {
+        '^/api/': '/'
+      }
+    }
   },
   /*
   ** Build configuration
