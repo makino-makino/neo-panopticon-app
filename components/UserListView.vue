95e55b87ba1a339e15e696125dff037e3515b93b
akakou <akama@keio.jp>
2019-11-14 21:07:49 +0900

    usersコンポーネントの追加

---
+++ components/UserListView.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+      <div v-for='user in users'> 
+        <UserView v-bind:id="user.id"
+            v-bind:name="user.name"
+            v-bind:bio="user.bio"
+            v-bind:icon="user.icon"
+            v-bind:evaluation="user.evaluation" />
+      </div> 
+
+  </div>
+</template>
+
+<script>
+
+import UserView from '~/components/UserView.vue'
+import axios from 'axios'
+
+
+const USERS_API = '/api/users/'
+
+export default {
+  components: {
+    UserView
+  },
+  props: {
+    query: String
+  },
+  async mounted () {
+    const HEADERS = {
+      'Accept': 'application/json',
+      'access-token': localStorage.access_token,
+      'client': localStorage.client,
+      'uid': localStorage.uid
+    }
+
+    var resp = await axios.get(
+      `${USERS_API}?${this.query}`,
+      {
+        headers: HEADERS
+      }
+    )
+
+    this.users = resp.data
+  },
+  data() {
+    return {
+      users: []
+    }
+  },
+  methods: {
+    
+  }
+}
+
+
+
+</script>
+
+<style>
+/* css */
+</style>
---
+++ components/UserView.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <p>{{id}}</p>
+    <p>{{name}}</p>
+    <p>{{bio}}</p>
+    <p>{{icon}}</p>
+    <p>{{evaluation}}</p>
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios'
+
+const USER_API = '/api/users/'
+
+export default {
+  props: {
+    id: Number,
+    name: String,
+    bio: String,
+    icon: String,
+    evaluation: Number
+  },
+  async mounted () {
+
+  },
+  data() {
+    return {
+      user: {}
+    }
+  }
+
+}
+
+
+
+</script>
+
+<style>
+/* css */
+</style>
