c46df0f136d2128ea7aa7060bddb40513dd7073c
akakou <akama@keio.jp>
2019-11-15 14:32:43 +0900

    フォローボタンの追加

--- components/UserView.vue
+++ components/UserView.vue
@@ -5,6 +5,7 @@
     <p>{{bio}}</p>
     <p>{{icon}}</p>
     <p>{{evaluation}}</p>
+    <button v-on:click="follow(id)">follow</button>
   </div>
 </template>
 
@@ -13,6 +14,8 @@
 import axios from 'axios'
 
 const USER_API = '/api/users/'
+const FOLLOWINGS_API = '/api/followings'
+
 
 export default {
   props: {
@@ -29,6 +32,37 @@ export default {
     return {
       user: {}
     }
+  },
+  methods: {
+    async follow(user_id) {
+      console.log(user_id)
+      try {
+        const HEADERS = {
+          'Accept': 'application/json',
+          'access-token': localStorage.access_token,
+          'client': localStorage.client,
+          'uid': localStorage.uid
+        }
+
+        var resp = await axios.post(
+          FOLLOWINGS_API, 
+          {
+            to_id: user_id
+          },
+          {
+            headers: HEADERS,
+            
+          }
+        )
+
+        // TODO: ちゃんと次の場所にジャンプさせる
+        location.href = '/'
+
+      } catch (e) {
+        console.log(e)
+      }
+    }
+
   }
 
 }
