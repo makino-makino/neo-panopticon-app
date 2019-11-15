669a1436fb6d086322dbdb5e399602717404a854
akakou <akama@keio.jp>
2019-11-15 14:33:04 +0900

    ホームTLの追加

---
+++ pages/homeTLPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>    
+    <PostListView v-bind:query="query"/>
+  </div>
+</template>
+
+<script>
+
+import PostListView from '~/components/PostListView.vue'
+import axios from 'axios'
+
+
+export default {
+  components: {
+    PostListView
+  },
+  data: () => {
+    return {
+      posts: [],
+      query: 'tl=local'
+    }
+  },
+  methods: {
+    
+  }
+}
+
+
+
+</script>
+
+<style>
+/* css */
+</style>
