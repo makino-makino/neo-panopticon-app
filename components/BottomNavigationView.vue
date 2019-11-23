0bfb6168d5d18f930d32a9084d8743199528cb74
akakou <akakou571@gmail.com>
2019-11-24 06:32:45 +0900

    BottomNativationViewを追加

---
+++ components/BottomNavigationView.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div class="footer l-box is-center">
+      <div class="pure-button content-button">
+        <img src="/images/home.png" class="content-button-img" />
+      </div>
+      <div class="pure-button content-button">
+        <img src="/images/earth.png" class="content-button-img" />
+      </div>
+      <div class="pure-button content-button">
+        <img src="/images/bell.png" class="content-button-img" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+/*
+ * -- LAYOUT STYLES --
+ * These are some useful classes which I will need
+ */
+.l-box {
+  padding: 1em;
+}
+
+.l-box-lrg {
+  padding: 2em;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+}
+
+.is-center {
+  text-align: center;
+}
+
+/* This is the class used for the footer */
+.footer {
+  background: #111;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+}
+
+.content-button {
+  background: rgb(48, 48, 48);
+}
+
+.content-button-img {
+  width: 30px;
+  height: 30px;
+  display: inline-block;
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
     title: process.env.npm_package_name || "",
     meta: [
       { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { name: "viewport", content: "" },
       {
         hid: "description",
         name: "description",
