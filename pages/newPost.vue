53d9bb2ca0ae62a1d1d0da3b129b2d2c50ea32bd
akakou <akakou571@gmail.com>
2019-11-24 07:31:41 +0900

    投稿後の遷移ページを変更

--- pages/newPost.vue
+++ pages/newPost.vue
@@ -1,57 +1,47 @@
 <template>
   <div>
-    <textarea v-model="content"></textarea><br>
+    <textarea v-model="content"></textarea><br />
     <button v-on:click="submit">投稿</button>
   </div>
 </template>
 
 <script>
+import NavigationBar from "~/components/NavigationBar.vue";
+import axios from "axios";
 
-import NavigationBar from '~/components/NavigationBar.vue'
-import axios from 'axios'
-
-const POSTS_API = '/api/posts'
-
+const POSTS_API = "/api/posts";
 
 export default {
-  components: {
-  },
+  components: {},
   data() {
     return {
-      content: ''
-    }
+      content: ""
+    };
   },
 
   methods: {
-    async submit (e) {
+    async submit(e) {
       const HEADERS = {
-        'Accept': 'application/json',
-        'access-token': localStorage.access_token,
-        'client': localStorage.client,
-        'uid': localStorage.uid
-      }
+        Accept: "application/json",
+        "access-token": localStorage.access_token,
+        client: localStorage.client,
+        uid: localStorage.uid
+      };
 
       var resp = await axios.post(
-        POSTS_API, 
+        POSTS_API,
         {
           content: this.content
         },
         {
-          headers: HEADERS,
-          
+          headers: HEADERS
         }
-      )
+      );
 
-      // TODO: ちゃんと次の場所にジャンプさせる
-      location.href = '/'
-
-
+      location.href = "/localTL";
     }
   }
-}
-
-
-
+};
 </script>
 
 <style>
