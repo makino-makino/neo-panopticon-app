69b5c2d500f30648bf783adfd59635ba49d2a47d
shunta nakajima <nakg_asimo@icloud.com>
2019-11-24 14:24:44 +0900

    とりあえずプロフィールで来た

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -1,26 +1,33 @@
 <template>
   <div>
-    <div v-if="amI">
-      <p>{{ user.id }}</p>
-      <p>{{ user.name }}</p>
-      <p>{{ user.bio }}</p>
-      <p>{{ user.icon }}</p>
-      <p>{{ user.evaluation }}</p>
-
-      <FollowButton v-bind:userId="user.id" />
+    <div class="profileBackGround">
+      <div v-if="amI">
+        <img src="/images/people.png" alt class="profileicon" />
+        <p class="name">{{ user.name }}</p>
+        <p class="bio">{{ user.bio }}</p>
+        <!-- <p>{{ user.id }}</p> -->
+        <!-- <p>{{ user.icon }}</p> -->
+        <!-- <p>{{ user.evaluation }}</p> -->
+      </div>
+      <div v-else>
+        <FollowButton v-bind:userId="user.id" />
+      </div>
     </div>
+    <PostListView v-bind:query="'tl=global'" />
   </div>
 </template>
 
 <script>
 import axios from "axios";
 import FollowButton from "~/components/FollowButton.vue";
+import PostListView from "~/components/PostListView.vue";
 
 const USERS_API = "/api/users";
 
 export default {
   components: {
-    FollowButton
+    FollowButton,
+    PostListView
   },
   validate({ params }) {
     return /^\d+$/.test(params.id);
@@ -59,6 +66,30 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
 /* css */
+.profileBackGround {
+  // height: 400px;
+  width: 100%;
+  background-color: rgb(31, 28, 41);
+  padding-top: 100px;
+  padding-bottom: 100px;
+  .profileicon {
+    height: 90px;
+    margin: 0 auto;
+    display: block;
+  }
+  .name {
+    text-align: center;
+    width: 50vw;
+    margin: 0 auto;
+  }
+  .bio {
+    text-align: center;
+    font-size: 0.9rem;
+    line-height: 0.9rem;
+    width: 50vw;
+    margin: 0 auto;
+  }
+}
 </style>
