5e60cdbf11723b86a0b61ca8f86538d136a22aab
shunta nakajima <nakg_asimo@icloud.com>
2019-11-25 03:55:14 +0900

    ログインページだけで来た

--- pages/login.vue
+++ pages/login.vue
@@ -1,12 +1,26 @@
 <template>
   <div>
-    <form>
-      メールアドレス<br />
-      <input v-model="email" type="email" /><br />
-      パスワード<br />
-      <input v-model="password" type="password" /><br />
-      <input v-on:click="login" value="送信" type="button" />
-    </form>
+    <div class="content">
+      <div class="inputs">
+        <div class="a_content">
+          <div></div>
+          <p>メールアドレス</p>
+          <div class="imagebox">
+            <img src="/images/mail.png" alt class="image" />
+          </div>
+          <input v-model="email" type="email" />
+        </div>
+        <div class="a_content">
+          <div></div>
+          <p>パスワード</p>
+          <div class="imagebox">
+            <img src="/images/key.svg" alt class="image" />
+          </div>
+          <input v-model="password" type="password" />
+        </div>
+      </div>
+      <input v-on:click="login" value="ログイン" type="button" class="submitbutton" />
+    </div>
   </div>
 </template>
 
@@ -40,12 +54,66 @@ export default {
 
       localStorage.userId = resp.data.data.id;
       // TODO: ちゃんと次の場所にジャンプさせる
-      this.$router.push('localTL')
+      this.$router.push("localTL");
     }
   }
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
 /* css */
+.a_content {
+  display: grid;
+  grid-template-columns: 60px 170px;
+  grid-template-rows: 45px;
+}
+.content {
+  background: url(/images/kagiana.svg);
+  height: calc(100vh - 59.78px);
+  background-position: bottom;
+  background-size: 44vh;
+  background-repeat: no-repeat;
+}
+p {
+  color: #4e4d4d;
+  font-size: 16px;
+  margin: 0;
+}
+img {
+  height: 30px;
+  margin: auto;
+}
+.imagebox {
+  width: 30px;
+  height: 30px;
+  display: flex;
+  margin: auto;
+}
+input {
+  width: 100%;
+  height: 40px;
+  background-color: #efefef;
+  border: 3px solid #4e4d4d;
+  border-radius: 6px;
+  font-size: 20px;
+  padding-left: 10px;
+  color: black;
+}
+.inputs {
+  margin: 0 auto;
+  display: table;
+  padding-top: 40vw;
+}
+.submitbutton {
+  width: 100px;
+  height: 40px;
+  border: none;
+  border-radius: 0;
+  background-color: #777777;
+  margin: 0 auto;
+  display: block;
+  margin-top: 26px;
+  font-size: 16px;
+  line-height: 100%;
+}
 </style>
