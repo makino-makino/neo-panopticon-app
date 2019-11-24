8e60fb34a6d766aefbb0e28db1cc8bfdad133619
shunta nakajima <nakg_asimo@icloud.com>
2019-11-25 00:17:22 +0900

    プロフィール更新画面

--- components/setting/profile.vue
+++ components/setting/profile.vue
@@ -1,9 +1,21 @@
 <template>
   <div class="allpage">
-    <NavigationBar :backbutton="true" :title="'プロフィール'" @closemenu="close"/>
-    <div class="content">
-      <button @click="close">ああああ</button>
-      これが最強のプロフィールコンポーネント
+    <NavigationBar :backbutton="true" :title="'プロフィール'" @closemenu="close" />
+    <div class="contents">
+      <div class="image_change">
+        <button>
+          <img src="/images/people.png" alt />
+        </button>
+      </div>
+      <div class="name">
+        <div class="boxname">名前</div>
+        <input type="text" />
+      </div>
+      <div class="detail">
+        <div class="boxname">自己紹介文</div>
+        <textarea></textarea>
+      </div>
+      <button class="save">保存</button>
     </div>
   </div>
 </template>
@@ -33,8 +45,49 @@ export default {
   left: 0;
   right: 0;
   background-color: rgb(48, 48, 48);
-  .content{
-
+  .contents {
+    padding: 0 10vw;
+    .image_change {
+      margin: 40px 0;
+      button {
+        margin: 0 auto;
+        display: block;
+        img {
+          height: 120px;
+        }
+      }
+    }
+    .boxname {
+      font-size: 20px;
+      line-height: 20px;
+    }
+    input {
+      width: 100%;
+      background-color: #dbdbdb;
+      color: black;
+      height: 35px;
+      border-radius: 6px;
+      font-size: 16px;
+      padding-left: 10px;
+    }
+    textarea {
+      width: 100%;
+      height: 30vh;
+      border: #111111 solid 1px;
+    }
+    .save {
+      height: 40px;
+      width: 100px;
+      background-color: #777777;
+      font-size: 20px;
+      line-height: 40px;
+      margin-top: 20px;
+      display: block;
+      margin-left: auto;
+    }
+  }
+  .detail {
+    margin-top: 40px;
   }
 }
 </style>
