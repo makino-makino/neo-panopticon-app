e99b2b5dc93adc7c6683a33870c25e20c26d9cc3
akakou <akakou571@gmail.com>
2019-11-24 11:57:31 +0900

    UserViewの実装を、FollowButton、UserBlockを使ったものにした

--- components/UserView.vue
+++ components/UserView.vue
@@ -1,19 +1,18 @@
 <template>
   <div>
-    <p>{{ id }}</p>
-    <p>{{ name }}</p>
-    <p>{{ bio }}</p>
-    <p>{{ icon }}</p>
-    <p>{{ evaluation }}</p>
-    <button v-on:click="follow(id)">follow</button>
+    <UserBlock v-bind:userId="id" />
+
+    <div class="follow-button-block">
+      <FollowButton v-bind:userId="id" />
+    </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
 
-const USER_API = "/api/users/";
-const FOLLOWINGS_API = "/api/followings";
+import FollowButton from "~/components/FollowButton.vue";
+import UserBlock from "~/components/UserBlock.vue";
 
 export default {
   props: {
@@ -23,34 +22,15 @@ export default {
     icon: String,
     evaluation: Number
   },
+  components: {
+    FollowButton,
+    UserBlock
+  },
   async mounted() {},
   data() {
     return {
       user: {}
     };
-  },
-  methods: {
-    async follow(user_id) {
-      const HEADERS = {
-        Accept: "application/json",
-        "access-token": localStorage.access_token,
-        client: localStorage.client,
-        uid: localStorage.uid
-      };
-
-      var resp = await axios.post(
-        FOLLOWINGS_API,
-        {
-          to_id: user_id
-        },
-        {
-          headers: HEADERS
-        }
-      );
-
-      // TODO: ちゃんと次の場所にジャンプさせる
-      location.href = "/";
-    }
   }
 };
 </script>
