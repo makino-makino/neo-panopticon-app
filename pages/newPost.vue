128f5dfce39cd0b2c33504292e998ce32126bbd4
shunta nakajima <nakg_asimo@icloud.com>
2019-11-24 14:36:55 +0900

    投稿ページを整えた　

--- pages/newPost.vue
+++ pages/newPost.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
-    <textarea v-model="content"></textarea><br />
-    <button v-on:click="submit">投稿</button>
+    <h2>新しい投稿を作成する</h2>
+    <textarea v-model="content"></textarea>
+    <button class="pure-button button" v-on:click="submit">投稿</button>
   </div>
 </template>
 
@@ -44,6 +45,27 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
 /* css */
+h2 {
+  font-size: 10rem;
+  font-size: 1.5rem;
+  letter-spacing: -2px;
+  width: 300px;
+  margin: 0 auto;
+  color: white;
+  margin-top: 35px;
+}
+textarea {
+  display: block;
+  border: white solid 1px;
+  margin: 0 auto;
+  margin-top: 19px;
+  width: 300px;
+  height: 200px;
+}
+.button {
+  margin: 0 auto;
+  display: block;
+}
 </style>
