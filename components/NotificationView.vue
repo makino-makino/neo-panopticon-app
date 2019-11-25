ab852680b3ef1e2043ee0fa1bd00a5093093eeda
shunta nakajima <nakg_asimo@icloud.com>
2019-11-25 11:27:35 +0900

    通知のあれをシた

--- components/NotificationView.vue
+++ components/NotificationView.vue
@@ -3,7 +3,7 @@
     <div class="notification" v-if="notification.type=='followed'">
       <div class="user_content">
         <div class="user_iconImage">
-          <img class="iconimage" src="/images/people.png" alt />
+          <img class="iconimage" :src="this.notification.user.icon" alt />
           <img class="followimage" src="/images/following.png" alt />
         </div>
       </div>
@@ -14,11 +14,11 @@
     <div class="notification" v-if="notification.type=='replied'">
       <div class="user_content">
         <div class="user_iconImage">
-          <img class="iconimage" src="/images/people.png" alt />
+          <img class="iconimage" :src="this.notification.post.icon" alt />
         </div>
       </div>
       <div class="notification_content">
-        <p>{{notification.user.name}}さんにフォローされました。</p>
+        <p>{{notification.post.name}}さんからのリプライ<br>{{notification.post.content}}</p>
       </div>
     </div>
   </div>
@@ -32,6 +32,15 @@ import axios from "axios";
 export default {
   props: ["notification"],
   async mounted() {
+    if (this.notification.type == "replied"){
+      if (this.notification.post.icon == null  || this.notification.post.icon == "") {
+        this.notification.post.icon = "/images/people.png";
+      }
+    }else{
+      if (this.notification.user.icon == null  || this.notification.user.icon == "") {
+        this.notification.user.icon = "/images/people.png";
+      }
+    }
     const HEADERS = {
       Accept: "application/json",
       "access-token": localStorage.access_token,
